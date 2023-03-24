f1e5762dde3f873d7583400881db3549ba6a8614
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-24 17:57:38 +0100

    insetito titolo e jumbo

---
+++ src/components/AppHero.vue
@@ -0,0 +1,15 @@
+<template>
+    <img src="https://pixabay.com/get/gead76e55e9fb43d6515c0d16a055d9251f361fa4de2c5e2a08ae4eaf511f129da933a03c3a3cea2bfd51fab4726b423840310a0224dcc179589610b973c5383e8dd75520fbeecc65f42f9bc2b66eeba4_1920.jpg"
+        alt="">
+</template>
+
+
+<script>
+
+</script>
+
+<style  scoped>
+img {
+    max-width: 800px;
+}
+</style>
